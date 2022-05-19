d851f61a387af81affb4f6da6699c56863c569a1
Muhammad-Saifullah-Sani <saiful19@mhs.unsyiah.ac.id>
2022-05-19 21:35:10 +0700

    Sedikit perbaikan

--- app/src/main/java/com/pbm/zakataceh/UserHomeFragment.java
+++ app/src/main/java/com/pbm/zakataceh/UserHomeFragment.java
@@ -57,7 +57,7 @@ public class UserHomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam2 = getArgudments().getString(ARG_PARAM2);
         }
     }
 
