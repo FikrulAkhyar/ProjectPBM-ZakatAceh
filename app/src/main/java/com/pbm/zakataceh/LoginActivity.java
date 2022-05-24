1f10666993f828d3c81c2fd52e11bf3161ae63e2
FikrulAkhyar <72435247+FikrulAkhyar@users.noreply.github.com>
2022-05-25 01:28:45 +0700

    "hubungkan login dan signup"

--- app/src/main/java/com/pbm/zakataceh/LoginActivity.java
+++ app/src/main/java/com/pbm/zakataceh/LoginActivity.java
@@ -2,13 +2,28 @@ package com.pbm.zakataceh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.pbm.zakataceh.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.linkDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/pbm/zakataceh/SignupActivity.java
+++ app/src/main/java/com/pbm/zakataceh/SignupActivity.java
@@ -2,13 +2,28 @@ package com.pbm.zakataceh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.pbm.zakataceh.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
 
+    ActivitySignupBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.linkMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
     }
 }
